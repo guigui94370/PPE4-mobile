f940e969da2099bac83264c8de46d1667f45c179
guigui94370 <guillaume.boudy@gmail.com>
2015-02-24 12:02:40 +0100

    Export du .apk + debug
    Bug résolu concernant l'ouverture du fichier local
    Création du .apk permettant de l'installer ;)
    le .apk se nomme app-release.apk il se situe dans le dossier /app/

--- app/src/main/java/com/guillaumeboudy/gtapp/MyActivity.java
+++ app/src/main/java/com/guillaumeboudy/gtapp/MyActivity.java
@@ -21,7 +21,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import java.net.UnknownHostException;
 
 
 //assetfile descriptor
